3f661f7a9ab8f814388ce3d260c6d4809b1231d3
Serkan Aksit <5222368+sekodev@users.noreply.github.com>
2020-06-29 18:25:31 +0300

    Changed branding to Solar2D

--- run_project.py
+++ run_project.py
@@ -1,5 +1,5 @@
 #
-# Sublime Text plugin to support Corona Editor
+# Sublime Text plugin to support Solar2D Editor
 #
 # Copyright (c) 2013 Corona Labs Inc. A mobile development software company. All rights reserved.
 #
